87d2c691f64095b9363a38f1b6e796df88821cb6
rozanovz <rozanovz13@gmail.com>
2015-06-24 23:38:46 +0300

    contact page without styling

--- LexelorNewN/js/headingTyped.js
+++ LexelorNewN/js/headingTyped.js
@@ -1,6 +1,11 @@
 $(function(){
-    $(".typed").typed({
+    $(".typedTeam").typed({
       strings: ["> Lexelor team"],
+      typeSpeed: 0,
+      showCursor: false
+    });
+    $(".typedContact").typed({
+      strings: ["> Contact Lexelor"],
       typeSpeed: 0,
       showCursor: false
     });
